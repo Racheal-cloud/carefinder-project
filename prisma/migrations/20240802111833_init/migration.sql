ccaa6f089f34a7ade8b8e122bc2dcc2d4bd62942
Racheal Abosede <ajiboyeabosederacheal@gmail.com>
2024-08-04 21:00:00 +0200

    update for database

--- prisma/migrations/20240802111833_init/migration.sql
+++ prisma/migrations/20240802111833_init/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "hospital" (
     "id" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
     "address" TEXT NOT NULL,
-    "phone" TEXT NOT NULL,
+    "contact" TEXT NOT NULL,
 
     CONSTRAINT "hospital_pkey" PRIMARY KEY ("id")
 );
